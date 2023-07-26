16e571c0c20c1b27b9cd903192447e7bdb13fb8c
David Anghel <david.anghel@nxp.com>
2023-07-26 15:15:41 +0300

    drivers: hse: Remove access to HSE RNG without proper initialization
    Reproduce the verification from `crypto_driver_init()`
    inside `hse_rng_read()` to stop TEE from trying to
    send requests to HSE without it being initialized
    first.
    
    Issue: ALB-10333, ALB-10654
    Signed-off-by: David Anghel <david.anghel@nxp.com>

--- core/drivers/crypto/hse/include/hse_core.h
+++ core/drivers/crypto/hse/include/hse_core.h
@@ -43,6 +43,8 @@ TEE_Result hse_buf_copy(struct hse_buf *dst, struct hse_buf *src, size_t size);
 uint32_t hse_buf_get_size(struct hse_buf *buf);
 paddr_t hse_buf_get_paddr(struct hse_buf *buf);
 
+bool is_hse_status_ok(void);
+
 TEE_Result hse_srv_req_sync(uint8_t channel, const void *srv_desc);
 TEE_Result hse_srv_req_async(uint8_t channel, const void *srv_desc,
 			     void *ctx,
